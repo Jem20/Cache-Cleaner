acc543db27465afd90c04d38666eb3d282dfbff4
Jem <68351730+Jem20@users.noreply.github.com>
2021-10-05 09:29:35 -0400

    Added the main file.

---
+++ Cache Cleaner.bat
@@ -0,0 +1,13 @@
+@echo off
+del /s /f /q c:\windows\temp\*.*
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+cls
